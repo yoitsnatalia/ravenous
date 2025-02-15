f894e8d397a768ec409e50ae1e3260c0d7dec60e
Natalia Linn <natalial@stanford.edu>
2025-02-15 14:20:12 -0800

    sortBy state functional with styles

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
 // sorting and searching section
@@ -8,11 +8,18 @@ export default function SearchBar() {
         "Highest Rated": "highest-rated",
         "Most Reviewed": "most-reviewed"
     };
+
+    const [searchTerms, setSearchTerms] = useState("");
+    const [location, setLocation] = useState("");
+    const [sortBy, setSortBy] = useState(sortOptions["Best Match"]);
+
     return (
         <div className="SearchBar">
             <div className="SortOptions">
+                {/* create a list object per sort option */}
                 <ul>{Object.keys(sortOptions).map(option => (
-                    <li key={sortOptions[option]}>{option}</li>
+                    // keep track of which sort option is active
+                    <li className={sortBy === sortOptions[option] ? "active" : ""} key={sortOptions[option]} onClick={() => setSortBy(sortOptions[option])}>{option}</li>
                 ))}</ul>
             </div>
             <div className="SearchFields">
